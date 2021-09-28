9f9f8a3333923ce057d15ccd30c8b7563189c58b
Colby Ryan <colbyr.ryan@gmail.com>
2021-09-28 14:59:01 -0500

    Added a nav bar as well as content to multiple new pages

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,42 +1,20 @@
 import React from "react"
 import "./Kennel.css"
-import { AnimalCard } from "./animal/AnimalCard"
-import { EmployeeCard } from "./Employees/Employee"
-import { LocationCard } from "./Locations/Location"
-import { CustomerCard } from "./Customers/Customer"
-import { PropsAndState } from "./PropsAndState"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+
+const isAdmin = true;
+
+const myUser = {
+    name:"Colby",
+    pet:"Dog"
+}
 
 export const Kennel = () => {
     return (
         <>
-            <h2>Nashville Kennels</h2>
-            <small>Loving care when you're not there.</small>
-            <address>
-                <div>Visit Us at the Nashville North Location</div>
-                <div>500 Puppy Way</div>
-            </address>
-            <PropsAndState yourName="Colby" day="Monday"/>  
-            <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
-        <article className="employees">
-            <EmployeeCard />
-            <EmployeeCard />
-            <EmployeeCard />
-        </article>
-        <article className="locations">
-            <LocationCard />
-            <LocationCard />
-            <LocationCard />
-        </article>
-        <article className="customers">
-            <CustomerCard />
-            <CustomerCard />
-            <CustomerCard />
-        </article>
+            <NavBar />
+            <ApplicationViews isAdmin={isAdmin} myUser={myUser} />
         </>
     )
 }

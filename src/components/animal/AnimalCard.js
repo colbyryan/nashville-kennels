7bf4aa972e3d46e409f2b5a09a18945cfc817f03
Colby Ryan <colbyr.ryan@gmail.com>
2021-10-06 11:00:41 -0500

    fixed errors, and got the features working

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -7,15 +7,17 @@ import { useHistory } from "react-router";
 
 export const AnimalCard = ({ animal, handleDeleteAnimal }) => {
     const history = useHistory();
-    <section className="animal">
-        <h3 className="animal__name">{firstLetterCase(animal.name)}</h3>
-        <div className="animal__breed">Breed: {animal.breed}</div>
-        <button type="button" onClick={() => handleDeleteAnimal(animal.id)}>Discharge</button>
-        <Link to={`/animals/${animal.id}`}><button>Details</button></Link>
-        <button type="button"
-            onClick={() => history.push(`/animals/${animal.id}/edit`)}>
-            Edit
-        </button>
+    return (
+        <section className="animal">
+            <h3 className="animal__name">{firstLetterCase(animal.name)}</h3>
+            <div className="animal__breed">Breed: {animal.breed}</div>
+            <button type="button" onClick={() => handleDeleteAnimal(animal.id)}>Discharge</button>
+            <Link to={`/animals/${animal.id}`}><button>Details</button></Link>
+            <button type="button"
+                onClick={() => history.push(`/animals/${animal.id}/edit`)}>
+                Edit
+            </button>
 
-    </section>
+        </section>
+    )
 }
